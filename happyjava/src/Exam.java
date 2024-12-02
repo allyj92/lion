90aad3c3944b5d7f03787bd47e960a6644c26da5
eunjaelim <imeunjae361@gmail.com>
2024-12-02 16:33:05 +0900

    while

---
+++ happyjava/src/Exam.java
@@ -0,0 +1,11 @@
+public class Exam {
+    public static void main(String[] args) {
+        System.out.println("김정미");
+//        System.out.println("hdsfd");
+
+        // 한줄 주석
+        /*
+        *
+        * */
+    }
+}
---
+++ happyjava/src/day01/DocTest.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class DocTest {
+    int age;
+    public static void main(String[] args) {
+
+
+       int i = 1;
+
+       while(i<=10){
+           System.out.println("**********");
+           i+=1;
+       }
+    }
+}
